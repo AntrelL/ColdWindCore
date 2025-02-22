95226e6529621281befa8b0a6f6f00a6c3572696
AntrelL <Nikitagaliant03@bk.ru>
2025-02-22 21:10:21 +0300

    Add string extensions

---
+++ Runtime/Misc/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+namespace ColdWind.Core
+{
+    public static class StringExtensions
+    {
+        public static string Remove(this string source, string substring)
+        {
+            return source.Replace(substring, string.Empty);
+        }
+    }
+}
